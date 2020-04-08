a40c334661f2396e37460132b3aa7f44ad79d078
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-04-08 21:51:10 +0300

    new file

--- Stacks_and_queues/maximum_and_minimum_element.py
+++ Stacks_and_queues/maximum_and_minimum_element.py
@@ -2,4 +2,20 @@ n = int(input())
 
 stack = []
 
+for i in range(n):
+    args = input().split(' ')
+    msg_type = args[0]
+    if msg_type == '1':
+        element = int(args[1])
+        stack.append(element)
+    elif msg_type == '2':
+        if len(stack) > 0:
+            stack.pop()
+    elif msg_type == '3':
+        print(max(stack))
+    elif msg_type == '4':
+        print(min(stack))
 
+for j in range(len(stack) - 1):
+    print(stack.pop(), end=', ')
+print(stack.pop())
