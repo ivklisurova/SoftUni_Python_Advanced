115cd08856addc6b87b0758c94eb12635409c030
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-04-12 00:18:10 +0300

    update variable

--- Stacks_and_queues/balanced_parentheses.py
+++ Stacks_and_queues/balanced_parentheses.py
@@ -8,16 +8,15 @@ for i in text:
     if i in '({[':
         stack.append(i)
     if i in ')}]':
-        a = stack[-1]
         if len(stack) > 0:
             b = stack.pop()
             if i == ')' and b != '(':
                 is_balanced = False
                 break
-            elif i == '}' and a != '{':
+            elif i == '}' and b != '{':
                 is_balanced = False
                 break
-            elif i == ']' and a != '[':
+            elif i == ']' and b != '[':
                 is_balanced = False
                 break
         else:
