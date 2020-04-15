cb6d8b61074b2ce5488b809668fafe06a755399c
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-04-16 00:09:57 +0300

    format

--- Tuples_and_Sets/softuni_party.py
+++ Tuples_and_Sets/softuni_party.py
@@ -14,9 +14,8 @@ while True:
     guests.add(guest)
 
 diff = list(guests ^ reservations)
-sorted_diff = sorted(diff,key=lambda x: x)
+sorted_diff = sorted(diff, key=lambda x: x)
 print(len(diff))
 
 [print(i) for i in sorted_diff if i[0].isdigit()]
 [print(j) for j in sorted_diff if j[0].isalpha()]
-
