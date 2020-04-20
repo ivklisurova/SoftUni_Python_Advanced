10c34f8208b057a57ed7cb6936b4ad3506e7c7e1
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-04-20 20:05:24 +0300

    change

--- Multidimensional_lists/2x2_squares_in_matrix.py
+++ Multidimensional_lists/2x2_squares_in_matrix.py
@@ -1,17 +1,17 @@
 rows, cols = map(int, input().split(' '))
 
-matrix = [input().split(' ') for i in range(rows)]
+matrix = [input().split(' ') for _ in range(rows)]
 
 equals = 0
 
-if rows >= 2 and cols >= 2:
-    for row in range(len(matrix) - 1):
-        for element in range(len(matrix[row]) - 1):
-            submatrx = matrix[row][element], matrix[row][element + 1], matrix[row + 1][element], matrix[row + 1][
-                element + 1]
-            if matrix[row][element] == matrix[row][element + 1] == matrix[row + 1][element] == matrix[row + 1][
-                element + 1]:
-                equals += 1
 
-    print(equals)
+for row in range(rows-1):
+    for element in range(cols-1):
+        submatrx = matrix[row][element], matrix[row][element + 1], matrix[row + 1][element], matrix[row + 1][
+            element + 1]
+        if matrix[row][element] == matrix[row][element + 1] == matrix[row + 1][element] == matrix[row + 1][
+            element + 1]:
+            equals += 1
+
+print(equals)
 
