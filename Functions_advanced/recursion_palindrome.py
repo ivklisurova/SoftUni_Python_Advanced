24f2cb657f237dd97c48392fcd5e42d06e6fc4d8
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-04-27 23:34:19 +0300

    new problems

---
+++ Functions_advanced/recursion_palindrome.py
@@ -0,0 +1,12 @@
+def palindrome(text, idx):
+    second_idx = len(text) - 1 - idx
+    if idx == len(text) // 2:
+        return f'{text} is a palindrome'
+    if text[idx] == text[second_idx]:
+        return palindrome(text, idx + 1)
+    else:
+        return f'{text} is not a palindrome'
+
+
+print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
