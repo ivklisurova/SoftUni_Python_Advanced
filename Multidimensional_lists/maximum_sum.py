176ae5048314f63e4a8f7a66de611ad439400f02
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-05-31 21:48:56 +0300

    update

--- Multidimensional_lists/maximum_sum.py
+++ Multidimensional_lists/maximum_sum.py
@@ -1,25 +1,37 @@
-n, m = map(int, input().split(' '))
+import sys
 
-matrix = [list(map(int, input().split(' '))) for _ in range(n)]
+n, m = map(int, input().split())
 
-max_submatrix = []
-sum_submatrix = 0
+matrix = [list(map(int, input().split())) for _ in range(n)]
 
+sum_submatrix = -sys.maxsize
+best_start = None
 
-for row in range(n - 2):
-    for col in range(m - 2):
+for row in range(len(matrix) - 2):
+    for col in range(len(matrix[row]) - 2):
+
         fist_row = matrix[row][col], matrix[row][col + 1], matrix[row][col + 2]
         second_row = matrix[row + 1][col], matrix[row + 1][col + 1], matrix[row + 1][col + 2]
         third_row = matrix[row + 2][col], matrix[row + 2][col + 1], matrix[row + 2][col + 2]
-        current_sum = sum(fist_row) + sum(second_row) + sum(third_row)
+        current_sum = sum(fist_row)+sum(second_row)+sum(third_row)
+
         if current_sum > sum_submatrix:
-            max_submatrix.clear()
-            max_submatrix.append(list(fist_row))
-            max_submatrix.append(list(second_row))
-            max_submatrix.append(list(third_row))
             sum_submatrix = current_sum
+            best_start = (row, col)
+
+best_matrix = []
+
+rows, cols = best_start[0], best_start[1]
+
+for i in range(3):
+    best_matrix.append([])
+    current_column = cols
+    current_row = rows+i
+    for j in range(3):
+        best_matrix[i].append(matrix[current_row][current_column])
+        current_column += 1
 
 print(f'Sum = {sum_submatrix}')
 
-for i in max_submatrix:
-    print(*i, sep=' ')
+for i in best_matrix:
+    print(' '.join([str(j) for j in i]))
