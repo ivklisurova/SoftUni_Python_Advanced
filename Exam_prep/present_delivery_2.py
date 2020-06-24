4b6e479009e88e832f0329cf3cf07d572c75e0d9
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-06-24 22:12:24 +0300

    exam_problem_multidim lists

--- Exam_prep/present_delivery_2.py
+++ Exam_prep/present_delivery_2.py
@@ -1,65 +1,79 @@
-present_count = int(input())
-size = int(input())
-matrix = []
-dropped_presents = 0
-total_count = 0
-pos = []
-my_dict = {'up': [-1, 0], 'down': [1, 0], 'left': [0, -1], 'right': [0, 1]}
-for i in range(size):
-    line = input().split()
-    matrix.append(line)
-    for j in range(size):
-        if line[j] == 'S':
-            pos = [i, j]
+def get_nice_kids(matrix):
+    count = 0
+    for r in matrix:
+        for c in r:
+            if c == 'V':
+                count += 1
+    return count
+
+
+def get_location(matrix):
+    for row in range(len(matrix)):
+        if 'S' in matrix[row]:
+            location = (row, matrix[row].index('S'))
+            return location
+
+
+def solve(matrix, loc, old_loc, directions):
+    global count_presents
+    global visited_nice_kids
+    if matrix[loc[0]][loc[1]] == 'X':
+        matrix[loc[0]][loc[1]] = 'S'
+        matrix[old_loc[0]][old_loc[1]] = '-'
+    elif matrix[loc[0]][loc[1]] == 'V':
+        matrix[loc[0]][loc[1]] = 'S'
+        matrix[old_loc[0]][old_loc[1]] = '-'
+        count_presents -= 1
+        visited_nice_kids += 1
+    elif matrix[loc[0]][loc[1]] == 'C':
+        matrix[loc[0]][loc[1]] = 'S'
+        matrix[old_loc[0]][old_loc[1]] = '-'
+        for v in directions.values():
+            position = (loc[0] + v[0], loc[1] + v[1])
+            if matrix[position[0]][position[1]] in 'VX' and count_presents:
+                count_presents -= 1
+                if matrix[position[0]][position[1]] == 'V':
+                    visited_nice_kids += 1
+                matrix[position[0]][position[1]] = '-'
+    elif matrix[loc[0]][loc[1]] == '-':
+        matrix[loc[0]][loc[1]] = 'S'
+        matrix[old_loc[0]][old_loc[1]] = '-'
+    return matrix
+
+
+count_presents = int(input())
+n = int(input())
+
+neighborhood = [input().split() for x in range(n)]
+
+nice_kids = get_nice_kids(neighborhood)
+visited_nice_kids = 0
+
+directions = {
+    'up': (-1, 0),
+    'down': (1, 0),
+    'left': (0, -1),
+    'right': (0, 1),
+}
 
 while True:
     command = input()
     if command == 'Christmas morning':
-        [print(' '.join(x)) for x in matrix]
-        print(f'Good job, Santa! {dropped_presents} happy nice kid/s.')
         break
-    change_dir = my_dict[command]
-    new_row = pos[0] + change_dir[0]
-    new_col = pos[1] + change_dir[1]
-    if matrix[new_row][new_col] == 'X':
-        matrix[pos[0]][pos[1]] = '-'
-        pos = [new_row, new_col]
-        matrix[pos[0]][pos[1]] = 'S'
-    elif matrix[new_row][new_col] == 'V':
-
-        matrix[pos[0]][pos[1]] = '-'
-        pos = [new_row, new_col]
-        matrix[pos[0]][pos[1]] = 'S'
-        dropped_presents += 1
-
-    elif matrix[new_row][new_col] == '-':
-        matrix[pos[0]][pos[1]] = '-'
-        pos = [new_row, new_col]
-        matrix[pos[0]][pos[1]] = 'S'
-    elif matrix[new_row][new_col] == 'C':
-        matrix[pos[0]][pos[1]] = '-'
-        pos = [new_row, new_col]
-        matrix[pos[0]][pos[1]] = 'S'
-        row = pos[0]
-        col = pos[1]
-        while present_count != total_count:
-            if matrix[row - 1][col] in 'XV':
-                total_count += 1
-                dropped_presents += 1
-                matrix[row - 1][col] = '-'
-            if matrix[row + 1][col] in 'XV':
-                total_count += 1
-                dropped_presents += 1
-                matrix[row + 1][col] = '-'
-            if matrix[row][col - 1] in 'XV':
-                total_count += 1
-                dropped_presents += 1
-                matrix[row][col - 1] = '-'
-            if matrix[row][col + 1] in 'XV':
-                total_count += 1
-                dropped_presents += 1
-                matrix[row][col + 1] = '-'
+    if count_presents == 0:
+        break
+    santa_location = get_location(neighborhood)
+    new_location = (santa_location[0] + directions[command][0], santa_location[1] + directions[command][1])
+    solve(neighborhood, new_location, santa_location, directions)
+    if count_presents == 0 and nice_kids > visited_nice_kids:
         print('Santa ran out of presents!')
-        [print(' '.join(x)) for x in matrix]
-        print(f'No presents for {dropped_presents} nice kid/s.')
-        print(total_count)
+        break
+    elif count_presents == 0:
+        break
+
+[print(' '.join(x)) for x in neighborhood]
+
+if visited_nice_kids == nice_kids:
+    print(f'Good job, Santa! {nice_kids} happy nice kid/s.')
+else:
+    print(f'No presents for {nice_kids - visited_nice_kids} nice kid/s.')
